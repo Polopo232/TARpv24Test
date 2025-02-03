2f43197a3ef69ca072d89ce87b23a3d7ba24b310
Opilane TTHK <opilane@tthk.ee>
2025-02-03 16:36:50 +0200

    1111

---
+++ methodcall/methodcall/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace methodcall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main method");
+
+            Main1();
+        }
+        static void Main1()
+        {
+            Console.WriteLine("Main1 method");
+        }
+    }
+}
