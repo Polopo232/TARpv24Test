ed82789f3eeb13c09e50a19b09165055d387936e
Opilane TTHK <opilane@tthk.ee>
2025-02-05 15:18:06 +0200

    111

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            string ch = Console.ReadLine();
+
+            switch(ch)
+            {
+                case "a":
+                    Console.WriteLine("vokaal a");
+                    break;
+                
+                case "e":
+                    Console.WriteLine("vokaal v");
+                    break;
+                
+                case "i":
+                    Console.WriteLine("vokaal a");
+                    break;
+
+
+                default:
+                    Console.WriteLine("ei ole bla bla bla");
+                    break;
+            }
+
+        }
+    }
+}
