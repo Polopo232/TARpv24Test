5b34b5870fae1b282af8f086e569f395d5d7a4a2
Opilane TTHK <opilane@tthk.ee>
2025-02-05 14:53:10 +0200

    111

---
+++ EuroCalc/EuroCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace EuroCalc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var deuro = 2.575;
+            var maaR = 6378;
+            maaR *= 100000;
+
+            var Pmaa = 2 * 3.14 * maaR;
+            var Peuro = Pmaa / deuro;
+
+            Console.WriteLine(Pmaa + " " + Math.Round(Peuro, 3));
+            
+
+        }
+    }
+}
